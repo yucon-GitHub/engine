7f99621d3d97e6c7a85fd76edb87dea942903eed
yucon <1245090462@qq.com>
2019-06-03 16:35:18 +0800

    vue3-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,23 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
+    root: true,
+    env: {
+        node: true
+    },
+    'extends': [
+        'plugin:vue/essential',
+        '@vue/standard'
+    ],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'indent': ['error',4],
+        "space-before-function-paren": 0,  // 函数定义时括号前面要不要有空格
+        "eol-last": 0,  // 文件以单一的换行符结束
+        "no-extra-semi": 0, // 可以多余的冒号
+        "semi": 0,  // 语句可以不需要分号结尾
+        "one-var": 0, // 连续声明
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <!--<img alt="Vue logo" src="../assets/logo.png">-->
+        <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+        <svg width="1000" height="1000" xmlns="http://www.w3.org/2000/svg" version="1.1">
+            <ellipse cx="200" cy="180" rx="80" ry="50" style="fill:rgba(255,255,255,0);stroke:#000;stroke-width:1" ref="boll" @click="aa" class="boll"/>
+            <text x="165" y="185" fill="#000">人工智能</text>
+            <!--<path d="M 280 260 q 200 450 80 0" stroke="#000" stroke-width="1" fill="none" />-->
+            <path d="M 160 225 L120 300 L280 300 L240 225" stroke="#000" stroke-width="1" fill="none" />
+            <path d="M 120 300 L100 350 L80 400 L90 450 L120 460 L280 460 L300 440 L320 400 L280 300" stroke="#000" stroke-width="1" fill="none" />
+            <path d="M 120 460 L120 520" stroke="#000" stroke-width="1" fill="none" />
+        </svg>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+    name: 'home',
+    components: {
+        // HelloWorld
+    },
+    data () {
+        return {
+        }
+    },
+    created () {
+        // this.$nextTick(()=>{
+        //     this.$refs.boll.style = 'fill:red;'
+        // })
+    },
+    methods: {
+        aa () {
+            this.$refs.boll.style = 'fill:red;'
+        }
+    }
+
 }
 </script>
+<style lang="scss" scoped>
+    .boll{
+        :hover{
+            fill:red;
+        }
+    }
+</style>
